04e126deb11a8ed72269d4138b96619fa7709c0f
Abhijith-Abi <abhijithabhijith1999@gmail.com>
2024-11-26 21:28:46 +0530

    Feat: add button and  change the favicon Changes to be committed: 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/Button.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import ButtonGradient from "./components/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+            <div>
+                <Button className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                    Something..
+                </Button>
+            </div>
+            <ButtonGradient />
         </>
     );
-}
+};
 
 export default App;
